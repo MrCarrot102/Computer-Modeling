fb9806bc49ee69f9350bbfe286623ddd1a7f2253
Mateusz Sierakowski <m.sierakowski@op.pl>
2025-02-26 22:47:25 +0100

    Działająca główna funkcja programu, problem z rysowaniem wykresu

--- monopoly/monopoly.cpp
+++ monopoly/monopoly.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <random> 
 #include <vector> 
+#include "matplotlibcpp.h"
 
+namespace plt = matplotlibcpp; _
 
-void Monopoly(int rolls, bool jail) { 
+void Monopoly(int rolls, bool jail, std::vector<double>& probabilites) { 
     const int board_size = 40; 
     std::vector<int> visits(board_size, 0); 
     int position = 0; 
@@ -20,16 +22,44 @@ void Monopoly(int rolls, bool jail) {
 
         if(jail && position == 30) { position = 10; } 
 
-        // std::cout << position << "\n";
-        
+        // std::cout << position << "\n"; 
         visits[position]++; 
+    }
+
+    for(int i = 0; i < board_size; ++i){
+        probabilites[i] = static_cast<double>(visits[i]) / rolls; 
     }
 }
 
+void plot_probabilites(const std::vector<double>& probabilites, const std::string& title) { 
+    std::vector<int> x(probabilites.size());
+    for(int i = 0; i < probabilites.size(); ++i)
+        x[i] = i; 
+
+    plt::bar(x, probabilites); 
+    plt::title(title);
+    plt::xlabel("Pole planszy");
+    plt::ylabel("Prawdopodobieństwo p(i)");
+    plt::show();
+}
+
+
+
+
+
 int main() {
     int a = 100, b = 1'000'000; 
+    
+    std::vector<double> probabilites1(40,0.0);
+    std::vector<double> probabilites2(40,0.0);
+    // symulacja bez więzienia 
+    Monopoly(a, false, probabilites1);
+    plot_probabilites(probabilites_no_jail, "Ruch po planszy bez więzienia (100 rzutów)"); 
 
-    Monopoly(a, false); 
+
+
+    return 0; 
+
 }
 
 
