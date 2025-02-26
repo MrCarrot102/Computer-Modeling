f287b92c3299d6830dd6cb43e6fdade365370b29
Mateusz Sierakowski <m.sierakowski@op.pl>
2025-02-26 22:21:22 +0100

    Stworzenie głównej funkcji

--- monopoly/monopoly.cpp
+++ monopoly/monopoly.cpp
@@ -1,31 +1,35 @@
-#include <iostream> 
+#include <iostream>
 #include <random> 
 #include <vector> 
 
 
-
-void monopoly(int rolls, bool jail, std::vector<double>& probabilities) {
+void Monopoly(int rolls, bool jail) { 
     const int board_size = 40; 
+    std::vector<int> visits(board_size, 0); 
     int position = 0; 
-    std::vector<int> visits(board_size,0);
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<int> dice(1,6); 
+
+    std::random_device rd; 
+    std::mt19937 gen(rd()); 
+    std::uniform_int_distribution<int> dice(1, 6); 
+
 
     for(int i = 0; i < rolls; ++i){
-        int roll = dice(gen) + dice(gen);
+        int roll = dice(gen) + dice(gen); 
+
         position = (position + roll) % board_size; 
 
-        if(jail && position == 30) 
-            position = 10; 
-    
+        if(jail && position == 30) { position = 10; } 
 
-    
-        visits[position]++;
+        // std::cout << position << "\n";
+        
+        visits[position]++; 
     }
+}
 
-    for(int i = 0; i < board_size; ++i)
-        probabilities[i] = static_cast<double>(visits[i])/rolls; 
+int main() {
+    int a = 100, b = 1'000'000; 
+
+    Monopoly(a, false); 
 }
 
 
