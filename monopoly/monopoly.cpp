5878fc15dfd84618c5e8648845b24e2a4132726c
Mateusz Sierakowski <m.sierakowski@op.pl>
2025-02-26 13:00:30 +0100

    Dodanie głównej funkcji programy

--- monopoly/monopoly.cpp
+++ monopoly/monopoly.cpp
@@ -1,25 +1,31 @@
 #include <iostream> 
-#include <vector> 
 #include <random> 
-#include <fstream> 
+#include <vector> 
 
-std::vector<double> monopoly(long long rolls, bool withJail) {
-    std::vector<int> visits(40, 0); 
-    std::random_devce rd; 
-    std::mt19937 gen(rd()); 
-    std::uniform_int_distibution<int> dice(1, 6);
 
+
+void monopoly(int rolls, bool jail, std::vector<double>& probabilities) {
+    const int board_size = 40; 
     int position = 0; 
+    std::vector<int> visits(board_size,0);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dice(1,6); 
 
-    for(long long i = 0; i < rolls; ++i){
-        int rolls = dice(gen) + dice(gen); 
-        positon = (position + roll) % 40; 
+    for(int i = 0; i < rolls; ++i){
+        int roll = dice(gen) + dice(gen);
+        position = (position + roll) % board_size; 
 
-        if(withJail && position == 30){ 
-            position == 10; 
-        }
+        if(jail && position == 30) 
+            position = 10; 
+    
 
-        visits[position]++; 
+    
+        visits[position]++;
     }
+
+    for(int i = 0; i < board_size; ++i)
+        probabilities[i] = static_cast<double>(visits[i])/rolls; 
 }
+
 
