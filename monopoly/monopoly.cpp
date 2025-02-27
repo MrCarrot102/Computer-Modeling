86c24732448b620594b0793bccd6b1b6966c08a7
Mateusz Sierakowski <m.sierakowski@op.pl>
2025-02-27 12:47:59 +0100

    Dodanie pythona do rysowania wykresów

--- monopoly/monopoly.cpp
+++ monopoly/monopoly.cpp
@@ -1,65 +1,52 @@
 #include <iostream>
 #include <random> 
 #include <vector> 
-#include "matplotlibcpp.h"
+#include <fstream> 
 
-namespace plt = matplotlibcpp; _
-
-void Monopoly(int rolls, bool jail, std::vector<double>& probabilites) { 
-    const int board_size = 40; 
-    std::vector<int> visits(board_size, 0); 
+void Monopoly(int rolls, bool jail){
+    int board_size = 40;
+    int jail_position = 30; 
+    std::vector<int> visits(board_size, 0);
+    
     int position = 0; 
+
+    std::ofstream file("data.csv");
 
     std::random_device rd; 
     std::mt19937 gen(rd()); 
     std::uniform_int_distribution<int> dice(1, 6); 
 
-
-    for(int i = 0; i < rolls; ++i){
+    for(int i = 0; i < rolls; i++){
         int roll = dice(gen) + dice(gen); 
-
+        
         position = (position + roll) % board_size; 
-
-        if(jail && position == 30) { position = 10; } 
-
-        // std::cout << position << "\n"; 
+        
+        if(jail && position == jail_position){
+            visits[jail_position]++;
+            position = 10;
+        }
         visits[position]++; 
     }
 
-    for(int i = 0; i < board_size; ++i){
-        probabilites[i] = static_cast<double>(visits[i]) / rolls; 
+    for(int i = 0; i < board_size; i++){
+        file << i << "," << visits[i] << "\n";
     }
+    file.close(); 
 }
 
-void plot_probabilites(const std::vector<double>& probabilites, const std::string& title) { 
-    std::vector<int> x(probabilites.size());
-    for(int i = 0; i < probabilites.size(); ++i)
-        x[i] = i; 
-
-    plt::bar(x, probabilites); 
-    plt::title(title);
-    plt::xlabel("Pole planszy");
-    plt::ylabel("Prawdopodobieństwo p(i)");
-    plt::show();
-}
-
-
-
-
-
-int main() {
-    int a = 100, b = 1'000'000; 
+int main(){
+    int rolls1 = 100, rolls2 = 1'000'000; 
     
-    std::vector<double> probabilites1(40,0.0);
-    std::vector<double> probabilites2(40,0.0);
-    // symulacja bez więzienia 
-    Monopoly(a, false, probabilites1);
-    plot_probabilites(probabilites_no_jail, "Ruch po planszy bez więzienia (100 rzutów)"); 
-
-
+    // checking for jail = false 
+    Monopoly(rolls1, false); 
+   // Monopoly(rolls2, false); 
+    
+    // for jail = true
+    //Monopoly(rolls2, true);
+//    Monopoly(rolls2, true);
 
     return 0; 
-
 }
+
 
 
